165f49297f16be779d70a3cc59de1fb95603b06c
Vincent <develop@unipayment.io>
2022-06-21 10:13:53 +0800

    Update Startup.cs

--- UniPayment.Client.Example/Startup.cs
+++ UniPayment.Client.Example/Startup.cs
@@ -46,7 +46,7 @@ namespace UniPayment.Client.Example
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
